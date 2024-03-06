3658d203d2dd171d96bbfe698e893a99279565ee
molkaezzine268 <162473608+molkaezzine268@users.noreply.github.com>
2024-03-06 17:45:43 +0100

    Update marts__kpi__monthly_turnover.sql

--- dbt/models/marts/kpi/marts__kpi__monthly_turnover.sql
+++ dbt/models/marts/kpi/marts__kpi__monthly_turnover.sql
@@ -11,3 +11,26 @@ FROM
     {{ ref("intermediate__invoices") }} i
 GROUP BY
     date_trunc('month', i.issue_date)
+    
+WITH date_spine AS (
+  SELECT DISTINCT
+    generate_series(
+      (SELECT MIN(date_trunc('month', issue_date)) FROM {{ ref("intermediate__invoices") }}),
+      (SELECT MAX(date_trunc('month', issue_date)) FROM {{ ref("intermediate__invoices") }}),
+      INTERVAL '1 month' #mois d'actobte manquant
+    ) AS issue_month_date
+)
+
+SELECT
+  d.issue_month_date,
+  COALESCE(SUM(i.amount), 0) AS amount
+FROM
+  date_spine d
+LEFT JOIN
+  {{ ref("intermediate__invoices") }} i
+ON
+  date_trunc('month', i.issue_date) = d.issue_month_date
+GROUP BY
+  d.issue_month_date
+ORDER BY
+  d.issue_month_date;
